c39052ad583a7a234fde22b2eff621702bec7e9d
Cristian Beraha <cristian@cloudops.works>
2025-09-17 18:08:11 -0300

    PIP-28 chore: add boilerplate, update README, and refine module configuration

--- versions.tf
+++ versions.tf
@@ -8,11 +8,9 @@ terraform {
   required_version = ">= 1.3"
   # Complete with required providers for the module
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
     github= {
       source = "integrations/github"
+      version = "~> 5.30"
     }
   }
 }
